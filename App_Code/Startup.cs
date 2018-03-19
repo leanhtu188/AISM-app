f76a44a5884cf51847c0fe2d4469c404febc951f
leanhtu188 <leanhtu188@gmail.com>
2018-03-19 20:26:15 +0800

    First

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aism_app.Startup))]
+namespace aism_app
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
