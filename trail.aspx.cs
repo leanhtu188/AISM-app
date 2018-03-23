57fa539053a9ebc69f648b48704c4e949e4d44c0
leanhtu188 <leanhtu188@gmail.com>
2018-03-23 19:42:52 +0800

    route page

--- trail.aspx.cs
+++ trail.aspx.cs
@@ -7,8 +7,10 @@ using System.Web.UI.WebControls;
 
 public partial class trail : System.Web.UI.Page
 {
+    public int haineseFootfall;
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //do your shit here
+        haineseFootfall = 0;
     }
 }
